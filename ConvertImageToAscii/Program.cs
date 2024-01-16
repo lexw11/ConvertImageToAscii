990e7f91d9eaf2e1543dcb2289cbea8a8a0462cd
Alex Wright <36146160+lexw11@users.noreply.github.com>
2024-01-15 19:34:58 -0800

    Got program working

--- ConvertImageToAscii/Program.cs
+++ ConvertImageToAscii/Program.cs
@@ -8,26 +8,31 @@ namespace ConvertImageToAscii
 { 
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            Program program = new Program();
+            program.btnConvertToAscii_Click();
         }
 
-        private void btnConvertToAscii_Click(object sender, EventArgs e)
+        //private void btnConvertToAscii_Click(object sender, EventArgs e)
+        private void btnConvertToAscii_Click()
         {
-            btnConvertToAscii.Enabled = false;
+            //btnConvertToAscii.Enabled = false;
             // Load the Image from the specified path
-            Bitmap image = new Bitmap(txtPath.Text, true);
+            const string imagePath = @"C:\Users\wrigh\Downloads\Mike_Wazowski.png";
+            Bitmap image = new Bitmap(imagePath);
+            //Bitmap image = new Bitmap(imageURL, true);
             // Resize the image
             // I've use a trackBar to emulate Zoom In / zoom Out feature
             // This value sets the WIDTH, number of characters, of the text image
-            image = GetReSizedImage(image, this.trackBar.Value);
+            image = GetReSizedImage(image, 150);
             // Convert the resized image into ASCII
-            _Content = ConvertToAscii(image);
+            string _Content = ConvertToAscii(image);
             // Enclose the final string between <pre> tags to preserve its formatting
             // and load it in the browser control
-            browserMain.DocumentText = "<pre>" + _Content + "</pre>";
-            btnConvertToAscii.Enabled = true;
+            //browserMain.DocumentText = "<pre>" + _Content + "</pre>";
+            Console.WriteLine(_Content);
+            //btnConvertToAscii.Enabled = true;
         }
 
         private Bitmap GetReSizedImage(Bitmap inputBitmap, int asciiWidth)
@@ -68,7 +73,8 @@ namespace ConvertImageToAscii
                 }
                 if (!toggle)
                 {
-                    sb.Append("<BR>");
+                    //sb.Append("<BR>");
+                    sb.Append("\n");
                     toggle = true;
                 }
                 else
@@ -79,28 +85,29 @@ namespace ConvertImageToAscii
             return sb.ToString();
         }
 
-        private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", "&nbsp;" };
+        //private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", "&nbsp;" };
+        private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
 
-        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            saveFileDialog1.Filter = "Text File (*.txt)|.txt|HTML (*.htm)|.htm";
-            DialogResult diag = saveFileDialog1.ShowDialog();
-            if (diag == DialogResult.OK)
-            {
-                if (saveFileDialog1.FliterIndex == 1)
-                {
-                    _Content = _Content.Replace("&nbsp;", " ").Replace("<BR>", "\n");
-                }
-                else
-                {
-                    _Content = "<pre>" + _Content + "</pre>";
-                }
-                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
-                sw.Write(_Content);
-                sw.Flush();
-                sw.Close();
-            }
-        }
+        //private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    saveFileDialog1.Filter = "Text File (*.txt)|.txt|HTML (*.htm)|.htm";
+        //    DialogResult diag = saveFileDialog1.ShowDialog();
+        //    if (diag == DialogResult.OK)
+        //    {
+        //        if (saveFileDialog1.FilterIndex == 1)
+        //        {
+        //            _Content = _Content.Replace("&nbsp;", " ").Replace("<BR>", "\n");
+        //        }
+        //        else
+        //        {
+        //            _Content = "<pre>" + _Content + "</pre>";
+        //        }
+        //        StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+        //        sw.Write(_Content);
+        //        sw.Flush();
+        //        sw.Close();
+        //    }
+        //}
     }
 }
 
