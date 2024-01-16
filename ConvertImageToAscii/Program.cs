bd5bae668a4850fac5a08d816863fbd085ef56b9
Alex Wright <36146160+lexw11@users.noreply.github.com>
2024-01-15 20:25:45 -0800

    Cleaned up code

--- ConvertImageToAscii/Program.cs
+++ ConvertImageToAscii/Program.cs
@@ -14,36 +14,33 @@ namespace ConvertImageToAscii
             program.btnConvertToAscii_Click();
         }
 
-        //private void btnConvertToAscii_Click(object sender, EventArgs e)
+        private string _AsciiChars = " .-:*+=%@##";
+        //private string _AsciiChars = "##@%=+*:-. ";
+
         private void btnConvertToAscii_Click()
         {
-            //btnConvertToAscii.Enabled = false;
-            // Load the Image from the specified path
-            const string imagePath = @"C:\Users\wrigh\Downloads\Mike_Wazowski.png";
-            Bitmap image = new Bitmap(imagePath);
-            //Bitmap image = new Bitmap(imageURL, true);
-            // Resize the image
-            // I've use a trackBar to emulate Zoom In / zoom Out feature
-            // This value sets the WIDTH, number of characters, of the text image
+            //const string imagePath = @"C:\Users\wrigh\Downloads\Mike_Wazowski.png";
+            const string imagePath = @"C:\Users\wrigh\Downloads\pupper.jpg";
+
+            Bitmap image = new Bitmap(imagePath, true);
+
             image = GetReSizedImage(image, 150);
-            // Convert the resized image into ASCII
+
             string _Content = ConvertToAscii(image);
-            // Enclose the final string between <pre> tags to preserve its formatting
-            // and load it in the browser control
-            //browserMain.DocumentText = "<pre>" + _Content + "</pre>";
+
             Console.WriteLine(_Content);
-            //btnConvertToAscii.Enabled = true;
         }
 
         private Bitmap GetReSizedImage(Bitmap inputBitmap, int asciiWidth)
         {
             int asciiHeight = 0;
-            // Calculate the new Height of the image from its width
+
             asciiHeight = (int)Math.Ceiling((double)inputBitmap.Height * asciiWidth / inputBitmap.Width);
-            // Create a new Bitmap and define its resolution
+
             Bitmap result = new Bitmap(asciiWidth, asciiHeight);
+
             Graphics g = Graphics.FromImage((Image)result);
-            // The interpolation mode produces high quality images
+
             g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
             g.DrawImage(inputBitmap, 0, 0, asciiWidth, asciiHeight);
             g.Dispose();
@@ -53,28 +50,25 @@ namespace ConvertImageToAscii
         private string ConvertToAscii(Bitmap image)
         {
             Boolean toggle = false;
-            StringBuilder sb = new StringBuilder();
-            for (int h = 0; h < image.Height; h++)
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int row = 0; row < image.Height; row++)
             {
-                for (int w = 0; w < image.Width; w++)
+                for (int column = 0; column < image.Width; column++)
                 {
-                    Color pixelColor = image.GetPixel(w, h);
-                    // Average out the RGB components to find the Gray Color
-                    int red = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
-                    int green = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
-                    int blue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
-                    Color grayColor = Color.FromArgb(red, green, blue);
-                    // Use the toggle flag to minimize height-wise stretch
+                    Color pixelColor = image.GetPixel(column, row);
+
+                    int rgbAverage = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    //Color grayColor = Color.FromArgb(rgbAverage, rgbAverage, rgbAverage);
+
                     if (!toggle)
                     {
-                        int index = (grayColor.R * 10) / 255;
-                        sb.Append(_AsciiChars[index]);
+                        int index = Convert.ToInt32(rgbAverage * (_AsciiChars.Length - 1) / 255);
+                        stringBuilder.Append(_AsciiChars[index]);
                     }
                 }
                 if (!toggle)
                 {
-                    //sb.Append("<BR>");
-                    sb.Append("\n");
+                    stringBuilder.Append("\n");
                     toggle = true;
                 }
                 else
@@ -82,32 +76,9 @@ namespace ConvertImageToAscii
                     toggle = false;
                 }
             }
-            return sb.ToString();
+            return stringBuilder.ToString();
         }
 
-        //private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", "&nbsp;" };
-        private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
-
-        //private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        //{
-        //    saveFileDialog1.Filter = "Text File (*.txt)|.txt|HTML (*.htm)|.htm";
-        //    DialogResult diag = saveFileDialog1.ShowDialog();
-        //    if (diag == DialogResult.OK)
-        //    {
-        //        if (saveFileDialog1.FilterIndex == 1)
-        //        {
-        //            _Content = _Content.Replace("&nbsp;", " ").Replace("<BR>", "\n");
-        //        }
-        //        else
-        //        {
-        //            _Content = "<pre>" + _Content + "</pre>";
-        //        }
-        //        StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
-        //        sw.Write(_Content);
-        //        sw.Flush();
-        //        sw.Close();
-        //    }
-        //}
     }
 }
 
